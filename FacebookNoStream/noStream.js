a4e40a872fa1ba04a5716aeeaeac1d5de843ab92
Johannes Haasen <jo.haasen@gmail.com>
2014-05-13 16:12:23 +0200

    remove activitytracker

--- FacebookNoStream/noStream.js
+++ FacebookNoStream/noStream.js
@@ -5,6 +5,15 @@ function removeStream() {
     }
 }
 
+function removeActivities() {
+    var activities = document.getElementsByClassName("tickerActivityStories")[0];
+    if(activities) {
+        activities.parentElement.removeChild(activities);
+    }
+}
+
 removeStream();
+removeActivities();
 
 setInterval(function(){removeStream()},1000);
+setInterval(function(){removeActivities()},1000);
